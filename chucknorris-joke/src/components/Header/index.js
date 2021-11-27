0c480b71d6d8115d1f4d2ef80b35cab9a2688703
Caroline Guimaraes <cguimaraes@acesso.com>
2021-11-27 13:00:21 -0300

    Aula 6 - Almoço

--- chucknorris-joke/src/components/Header/index.js
+++ chucknorris-joke/src/components/Header/index.js
@@ -1,15 +1,49 @@
-import {
-    Menu
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {   
+    Grid,
+    GridItem,
+    Container,    
+    FormControl,
+    FormLabel,
+    Select
 } from '@chakra-ui/react'
-import Logo from '../../assets/logo.jpg'
+import Logo from '../../assets/logo.jpg';
+import api from '../../services/api';
 
 const Header = () => {
+    const [main, setMain] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('categories').then(
+            response => {
+                setMain(response.data)
+            }
+        )
+    }, [])
+    const handleCategory = (e) => {       
+        navigate(`/categories/${e.target.value}`)
+    }
     return(
-        <nav>           
-           <img src={Logo} className="logo" alt ="Logo"/>
-           <Menu>
-               
-           </Menu>
+        <nav>   
+            <Container maxW="container.xl">
+                <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+                    <GridItem colStart={1} colEnd={4}>
+                        <Link to ='/'>        
+                            <img src={Logo} className="logo" alt ="Logo"/>
+                        </Link>
+                    </GridItem>
+                    <FormControl>
+                        <FormLabel>Selecione a categoria da piada </FormLabel>
+                        <Select onChange={ handleCategory }>                        
+                            {main?.map( (item, index) => (                                
+                                <option key={index} value={item}> {item} </option>   
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+           </Container>
         </nav>
     )
 }
--- chucknorris-joke/src/views/Home/index.js
+++ chucknorris-joke/src/views/Home/index.js
@@ -1,21 +1,44 @@
 import React, {useEffect, useState} from 'react';
 import api from '../../services/api'
 import Loader from '../../assets/loader.gif';
+import {
+  FormLabel,
+  Input,
+  Button
+
+} from '@chakra-ui/react';
 
 const App = () => {
-  const [data, setData] = useState({}) 
+  const [data, setData] = useState({});
+  const [allJokes, setAllJokes] = useState({});
+  const [isSearch, setIsSearch] = useState(false); 
   const [isLoad, setIsLoad] = useState(false); 
+  const [searchJoke, setSearchJoke] = useState('');  
+
   useEffect(() => { //Responsável por iniciar o ciclo de vida, no array é o que ele vai observar na estrutura
     setIsLoad(true); 
     api.get('random').then(
        response => {
         setData(response.data)
       }
-     ).catch( e => console.error(e))
+     )
+     .catch( e => console.error(e))
      .finally(() => setTimeout(() => {
       setIsLoad(false)
-     },2500 ))
+     },2500))
   }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoad(true)
+    api.get(`search?query=${searchJoke}`).then(
+      response => {
+        setIsSearch(true)
+        setAllJokes(response.data)
+      }
+    ).catch(e => console.error(e))
+    .finally(() => setIsLoad(false))
+  }
 
   if (isLoad){
     return(
@@ -28,10 +51,28 @@ const App = () => {
   return(
     <div className="home-component">
       <h1>Joke </h1>
-      <div className="jokes">
-        <img src={data?.icon_url} alt={data?.icon_url} />
-        <h3>{data?.value}</h3>
+      <div>
+        <form onSubmit={handleSubmit}> 
+          <FormLabel>Pesquise sua piada </FormLabel>
+          <Input type="text" onChange={e => setSearchJoke(e.target.value)} />
+          <Button type="submit" colorScheme="blue">Pesquisar</Button>
+        </form>
       </div>
+      { !isSearch ? (
+        <div className="jokes">
+          <img src={data?.icon_url} alt={data?.icon_url} />
+          <h3>{data?.value}</h3>          
+        </div>
+      ) : (
+        <>
+        { allJokes?.result.map((item , index) => (
+          <div key={index} className="jokes">
+            <img src={item?.icon_url} alt={item?.icon_url} />
+            <h3>{item?.value}</h3>          
+          </div>
+        ))}
+        </>
+      )}
     </div>
   )
 }
